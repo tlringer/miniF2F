243186f04e9f4d5988046de19dd866235c8b13fc
Max Fan <root@max.fan>
2022-05-10 15:13:33 -0400

    Replace weaker translation with stronger, more accurate translation
    I didn't realize that even though this is given as a multiple choice
    problem, the other translations (e.g. in Lean) translate the sentence
    as a forall (rather than an existential) sentence. This fixes the issue
    and brings the translation in-line with the others.

--- coq/valid/amc12_2000_p11.v
+++ coq/valid/amc12_2000_p11.v
@@ -7,20 +7,16 @@ Infix "+" := Rplus.
 Infix "*" := Rmult.
 Infix "-" := Rminus.
 
-Lemma constructive_example : ((1%R <> 0%R /\ 0.5%R <> 0%R) ->
-    (1%R * 0.5%R = 1%R - 0.5%R) ->
-    (1%R / 0.5%R + 0.5%R / 1%R - 1%R * 0.5%R = 2%R)).
-Proof.
-  intros.
-  lra.
-Qed.
-
 Theorem amc_12_2000_p11 :
-  exists (a b : R), (a <> 0%R /\ b <> 0%R) ->
+  forall (a b : R), (a <> 0%R /\ b <> 0%R) ->
     (a * b = a - b) ->
     (a / b + b / a - a * b = 2%R).
 Proof.
-  eapply ex_intro.
-  eapply ex_intro.
-  apply constructive_example.
+  intros.
+  destruct H.
+  field_simplify_eq; auto.
+  replace (- a ^ 2 * b ^ 2) with ((a * b) * (a * b)* -1) by lra.
+  rewrite H0.
+  field_simplify_eq.
+  auto.
 Qed.
